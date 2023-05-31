44e43efd76d5c920a19cb0357d3573fc8bf163d5
nhduchuy2306 <nguyenhuy1323614@gmail.com>
2023-05-31 13:47:01 +0700

    bottom navigation bar

--- app/src/main/java/com/example/gearmobile/MainActivity.java
+++ app/src/main/java/com/example/gearmobile/MainActivity.java
@@ -1,32 +1,39 @@
 package com.example.gearmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
 
-import com.example.gearmobile.activities.LoginActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private Button button;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if(item.getItemId() == R.id.navigation_home) {
+                Toast.makeText(MainActivity.this, "Home Screen", Toast.LENGTH_SHORT).show();
             }
+            else if(item.getItemId() == R.id.navigation_explore) {
+                Toast.makeText(MainActivity.this, "Explore Screen", Toast.LENGTH_SHORT).show();
+            }
+            else if(item.getItemId() == R.id.navigation_cart) {
+                Toast.makeText(MainActivity.this, "Cart Screen", Toast.LENGTH_SHORT).show();
+            }
+            else if(item.getItemId() == R.id.navigation_chat) {
+                Toast.makeText(MainActivity.this, "Chat Screen", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(MainActivity.this, "Profile Screen", Toast.LENGTH_SHORT).show();
+            }
+            return true;
         });
     }
 }
