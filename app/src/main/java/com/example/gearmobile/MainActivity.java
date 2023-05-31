0871d4fbcbaacfabf1f8bfe2dcaf6d5e0daf016f
nhduchuy2306 <nguyenhuy1323614@gmail.com>
2023-05-31 09:32:00 +0700

    first commit

---
+++ app/src/main/java/com/example/gearmobile/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.gearmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
