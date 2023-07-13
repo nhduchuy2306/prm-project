109bbbd462c0004da45594a6cf6e2d15e404824e
Vuong Huu Hieu <ownhieu0804@gmail.com>
2023-07-13 21:10:30 +0700

    get userLocation

--- app/src/main/java/com/example/happygear/activities/AboutActivity.java
+++ app/src/main/java/com/example/happygear/activities/AboutActivity.java
@@ -1,10 +1,13 @@
 package com.example.happygear.activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -66,5 +69,6 @@ public class AboutActivity extends AppCompatActivity implements AddressListener 
         Intent intent = new Intent(this, MapsActivity.class);
         intent.putExtra("shopAddress", shopAddress);
         startActivity(intent);
+
     }
 }
